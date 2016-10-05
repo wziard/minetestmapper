13b485a75f0ed3818c32731279ec48491766ac1b
sfan5 <sfan5@live.de>
2016-10-05 23:06:05 +0200

    Update travis build to new container infrastructure (w/o sudo)

--- util/travis/script.sh
+++ util/travis/script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -e
 
+CXX=g++-6
+[ $CC == "clang" ] && CXX=clang-3.8
+export CXX
+
+
 mkdir -p travisbuild
 cd travisbuild
-cmake -DENABLE_LEVELDB=1 -DUSE_CXX11=$CXX11 ..
+
+cmake \
+	-DENABLE_LEVELDB=1 -DUSE_CXX11=$CXX11 \
+	-DLEVELDB_LIBRARY=../libleveldb/lib/libleveldb.so \
+	-DLEVELDB_INCLUDE_DIR=../libleveldb/include \
+	..
+
 make -j2
